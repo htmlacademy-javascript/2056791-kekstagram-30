e91756290aae7f844c47d0a6ab33f33dfc8ac74e
Сергей Даштамиров <cosmostar2013@rambler.ru>
2023-10-14 23:45:48 +0300

    Нужно больше функий

---
+++ js/functions.js
@@ -0,0 +1,54 @@
+function lengthCheck(str, length){
+  const SUCCES = `Проверка прошла: ${str}, символов ${length}`;
+  const NOTSUCCES = `Проверка не прошла ${str}, строка длиннее чем указано ${length}`;
+
+  let accesString = (str.length < length || str.length == length) ? SUCCES : NOTSUCCES;
+  return accesString;
+}
+
+console.log(lengthCheck('проверяемая строка', 20));
+
+console.log(lengthCheck('проверяемая строка', 18));
+
+console.log(lengthCheck('проверяемая строка', 10));
+
+// Палиндром
+function isPadindrom(str){
+  let cleanedStr = str.toLowerCase().replaceAll(" ", "");
+
+  let reversedStr = cleanedStr.split("").reverse().join("");
+
+  let result = (cleanedStr == reversedStr) ? true : false;
+
+  return result;
+}
+
+console.log(isPadindrom('топот'));
+console.log(isPadindrom('ДовОд'));
+console.log(isPadindrom('Кексcа'));
+console.log(isPadindrom('Лёша на полке клопа нашёл '));
+
+
+function numberRes(str){
+
+  let succes = "" + str
+  let reserch = succes.replaceAll(' ', '').split("");
+  let number = '';
+  for(let i = 0; i < reserch.length; i++){
+    if(reserch[i] == +reserch[i]){
+      number += reserch[i];
+    }
+  }
+
+  return parseInt(number);
+}
+
+console.log(numberRes('2023 год'));
+console.log(numberRes('ECMAScript 2022'));
+console.log(numberRes('1 кефир, 0.5 батона'));
+console.log(numberRes('агент 007'));
+console.log(numberRes(' я томат'));
+
+console.log(numberRes(2023));
+console.log(numberRes(-1));
+console.log(numberRes(1.5));
